31d8a3ad6400ae8038a7b79df1d5458bd28fedbc
aka <3027243797@qq.com>
2022-10-12 17:24:03 +0800

    thrid

--- src/main/java/com/node/GItTest2.java
+++ src/main/java/com/node/GItTest2.java
@@ -3,5 +3,6 @@ package com.node;
 public class GItTest2 {
     public static void main(String[] args) {
         System.out.println("tijiaobul");
+        System.out.println("cestijiao2");
     }
 }
